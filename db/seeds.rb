727710db77985f023e33ca804bbe387b68e20a24
manicalshawn <hansenpshawn@gmail.com>
2015-04-15 09:24:37 -0400

    seeds.

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,7 +8,8 @@
 
 def run_seed
 	make_user
-
+	make_articles
+	make_tags
 end
 
 def make_user
@@ -16,6 +17,21 @@ def make_user
 	User.create(:name => "Keith", :email => "keith@awesome.com", :article_rep => 900, :comment_rep => 400)
 	User.create(:name => "Ashley", :email => "ashley@awesome.com", :article_rep => 800, :comment_rep => 300)
 	User.create(:name => "Sanj", :email => "sanj@awesome.com", :article_rep => -1000, :comment_rep => -500 )
+end
+
+def make_articles
+	Article.create(:title => "Dank", :abstract => "lots of facts", :sources => "me", :body => "all the body", :rep => 10, :author_id => 1)
+	Article.create(:title => "Blank", :abstract => "lots of factss", :sources => "him", :body => "all the body", :rep => 5, :author_id => 4)
+end
+
+def make_tags
+	Tag.create(:name => "Best", :article_id => 1)
+	Tag.create(:name => "West", :article_id => 2)
+end
+
+def make_comments
+	Comment.create(:body => "Awesome!", :user_id => 3, :article_id => 1, :rep => 3)
+	Comment.create(:body => "Wowza", :user_id => 1, :article_id => 2, :rep => 0)
 end
 
 run_seed
