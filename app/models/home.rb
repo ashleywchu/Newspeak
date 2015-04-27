608d6fef723bfb8e35a96d308ce4680b14d5be6a
manicalshawn <hansenpshawn@gmail.com>
2015-04-27 16:12:31 -0400

    added home PORO

---
+++ app/models/home.rb
@@ -0,0 +1,18 @@
+class Home
+
+
+	def self.most_popular
+		all_articles.sort! {|p2, p1| p1.reputation_for(:votes) <=> p2.reputation_for(:votes)}
+	end
+	
+	private
+
+	def self.all_articles
+		User.all.each_with_object([]) do |user, array|
+			user.articles.each do |article|
+				array << article
+			end
+		end
+	end
+
+end
