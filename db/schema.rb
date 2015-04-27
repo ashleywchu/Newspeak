8d7b6477a77350f9b10d02d2317c727ec07167db
manicalshawn <hansenpshawn@gmail.com>
2015-04-27 13:49:02 -0400

    fixed search nav route.

--- db/schema.rb
+++ db/schema.rb
@@ -11,8 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 20150423200447) do
+ActiveRecord::Schema.define(version: 20150423200448) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
