1c268419ab4ab57b75ef95ea41387815c49f4987
manicalshawn <hansenpshawn@gmail.com>
2015-04-29 16:18:14 -0400

    actionmailer implemented, emails only new users, also fixed tag/seed data.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,8 +1,9 @@
 class SessionsController < ApplicationController
 
   def create
-    user = User.find_by_provider_and_uid(auth[:provider], auth[:uid]) || user = User.create_with_omniauth(auth)
-    session[:user_id] = user.id
+    user = User.find_by_provider_and_uid(auth[:provider], auth[:uid]) || new_user = User.create_with_omniauth(auth)
+    UserMailer.welcome_email(new_user).deliver_now if new_user
+    session[:user_id] = user.id || new_user.id
     redirect_to "/"
   end
 
---
+++ app/mailers/application_mailer.rb
@@ -0,0 +1,4 @@
+class ApplicationMailer < ActionMailer::Base
+  default from: "notice.newspeak@gmail.com"
+  layout 'mailer'
+end
